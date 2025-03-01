d9d9bb8c21ecdd4149d629a90bf7c958cbe104bc
thamaraikannan137 <thamaraikannan137@gmail.com>
2025-03-01 09:10:55 +0530

    commenting out all firebase related dependecies

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,47 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 WebViewEnvironment? webViewEnvironment;
 
-Future<void> handleBackgroundmessage(RemoteMessage message) async {
-  print('$message');
-  print('title : ${message.notification?.title}');
-  print('body : ${message.notification?.body}');
-  print('paylaod : ${message.data}');
-}
+// Future<void> handleBackgroundmessage(RemoteMessage message) async {
+//   print('$message');
+//   print('title : ${message.notification?.title}');
+//   print('body : ${message.notification?.body}');
+//   print('paylaod : ${message.data}');
+// }
 
-Future<void> initPushNotifications() async {
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true, badge: true, sound: true);
-  FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
-  FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
-  FirebaseMessaging.onBackgroundMessage(handleBackgroundmessage);
-}
+// Future<void> initPushNotifications() async {
+//   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+//       alert: true, badge: true, sound: true);
+//   FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+//   FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+//   FirebaseMessaging.onBackgroundMessage(handleBackgroundmessage);
+// }
 
-void handleMessage(RemoteMessage? message) {
-  print('>>>>>>>>>>');
-  if (message == null) return;
-  print('=============================================');
-  // navigatorKey.currentState
-  //     ?.pushNamed(NotificationScreen.route, arguments: message);
-}
+// void handleMessage(RemoteMessage? message) {
+//   print('>>>>>>>>>>');
+//   if (message == null) return;
+//   print('=============================================');
+//   // navigatorKey.currentState
+//   //     ?.pushNamed(NotificationScreen.route, arguments: message);
+// }
 
-class FirebaseApi {
-  final _firebaseMessaging = FirebaseMessaging.instance;
-
-  Future initNotifications() async {
-    NotificationSettings settings =
-        await _firebaseMessaging.requestPermission();
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {}
-    // final fcmToken = await _firebaseMessaging.getToken();
-    // print("Token : $fcmToken");
-    await _firebaseMessaging.subscribeToTopic('web_to_apk');
-    initPushNotifications();
-  }
-}
+// class FirebaseApi {
+//   final _firebaseMessaging = FirebaseMessaging.instance;
+//
+//   Future initNotifications() async {
+//     NotificationSettings settings =
+//         await _firebaseMessaging.requestPermission();
+//     if (settings.authorizationStatus == AuthorizationStatus.authorized) {}
+//     // final fcmToken = await _firebaseMessaging.getToken();
+//     // print("Token : $fcmToken");
+//     await _firebaseMessaging.subscribeToTopic('web_to_apk');
+//     initPushNotifications();
+//   }
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
